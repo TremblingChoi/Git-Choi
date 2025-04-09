b092a4c6cd51665b63d2e24551e838c2ce82c530
HyunJun Choi <smartchj0525@gmail.com>
2025-04-09 23:57:07 +0900

    Update hw5.py

--- hw5.py
+++ hw5.py
@@ -1,7 +1,25 @@
 def read_single_digit(d):
-    digit_to_korean = ["영", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"]
-    return digit_to_korean[d]
-
+    if d == 0:
+        return "영"
+    elif d == 1:
+        return "일"
+    elif d == 2:
+        return "이"
+    elif d == 3:
+        return "삼"
+    elif d == 4:
+        return "사"
+    elif d == 5:
+        return "오"
+    elif d == 6:
+        return "육"
+    elif d == 7:
+        return "칠"
+    elif d == 8:
+        return "팔"
+    elif d == 9:
+        return "구"
+        
 def read_number(n):
     num_str = str(n)
     return (
