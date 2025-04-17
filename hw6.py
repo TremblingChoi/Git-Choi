8b7174ae63017db31ba8fb6642395593c0d05aca
HyunJun Choi <smartchj0525@gmail.com>
2025-04-17 13:25:05 +0900

    Create hw6.py

---
+++ hw6.py
@@ -0,0 +1,15 @@
+def display_multiplication_table1():
+    for n in range(1,10):
+        for i in range(2, 6):
+            print(f'{i} x {n} = {n*i:2d}',end='\t')
+        print()
+
+def display_multiplication_table2():
+    for n in range(1,10):
+        for i in range(6, 10):
+            print(f'{i} x {n} = {n*i:2d}',end='\t')
+        print()
+
+display_multiplication_table1()
+print()
+display_multiplication_table2()
